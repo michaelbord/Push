3e8e349cb964ea91584265b7b980508042db2a18
michael <michael@LOOPING>
2016-11-17 21:49:02 +0100

    Ajout du controller de test

---
+++ src/Push/Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebPush.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+        private static int Count = 0;
+
+        // GET: api/values
+        [HttpGet]
+        public string Get()
+        {
+            Count++;
+            return Count.ToString();
+        }
+
+        [HttpGet]
+        [Route("display")]
+        public string Display()
+        {
+            return Count.ToString();
+        }
+
+    }
+}
